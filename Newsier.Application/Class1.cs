de786224db25fd52fbcc60e22a157b57a468eb63
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-01-21 18:57:01 +0200

    init

---
+++ Newsier.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Newsier.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Newsier.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Newsier.Domain
+{
+    public class Class1
+    {
+    }
+}
