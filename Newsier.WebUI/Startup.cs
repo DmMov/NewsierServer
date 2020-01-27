e8fb23d5d6b2b9893559ce4e26e1dfee4b1a20c4
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-01-27 21:26:22 +0200

    add infrastructure and application DI to Startup

--- Newsier.WebUI/Startup.cs
+++ Newsier.WebUI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newsier.Application;
+using Newsier.Infrastructure;
 
 namespace Newsier.WebUI
 {
@@ -22,13 +24,14 @@ namespace Newsier.WebUI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication();
+            services.AddInfrastructure();
+
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
