874b4c9d1e79a001b0bec1381cfb87adc0f4dd45
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-01-27 18:53:39 +0200

    delete all the Class1.cs files from all the class libraries

--- Newsier.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Newsier.Application
-{
-    public class Class1
-    {
-    }
-}
--- Newsier.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Newsier.Domain
-{
-    public class Class1
-    {
-    }
-}
