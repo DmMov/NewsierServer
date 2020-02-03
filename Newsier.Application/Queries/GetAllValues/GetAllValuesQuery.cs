ad2602b2ac324152d4e8a1b1775b8d5e634a5aab
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-02-03 21:04:21 +0200

    add values controller and get all values query for just a test

---
+++ Newsier.Application/Queries/GetAllValues/GetAllValuesQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Newsier.Application.Queries.GetAllValues
+{
+    public sealed class GetAllValuesQuery : IRequest<string[]>
+    {
+        public sealed class Handler : IRequestHandler<GetAllValuesQuery, string[]>
+        {
+            public Task<string[]> Handle(GetAllValuesQuery request, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(new string[] { "value 1", "value 2" });
+            }
+        }
+    }
+}
---
+++ Newsier.WebUI/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newsier.Application.Queries.GetAllValues;
+
+namespace Newsier.WebUI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> GetAll()
+        {
+            var query = new GetAllValuesQuery();
+            return Ok(await Mediator.Send(query));
+        }
+    }
+}
