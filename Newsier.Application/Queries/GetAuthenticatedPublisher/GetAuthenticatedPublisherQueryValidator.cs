4ff011f1ce5912ca979e010e5ac9380752219d6f
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-04-01 18:32:06 +0300

    Added a Validator for the GetAuthenticatedPublisherQuery

---
+++ Newsier.Application/Queries/GetAuthenticatedPublisher/GetAuthenticatedPublisherQueryValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Newsier.Application.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Newsier.Application.Queries.GetAuthenticatedPublisher
+{
+    public sealed class GetAuthenticatedPublisherQueryValidator : AbstractValidator<GetAuthenticatedPublisherQuery>
+    {
+        private readonly INewsierContext _context;
+
+        public GetAuthenticatedPublisherQueryValidator(INewsierContext context)
+        {
+            _context = context;
+
+            RuleFor(query => query.PublisherId)
+                .NotEmpty()
+                .WithMessage("'PublisherId' is null or an empty string")
+                .MustAsync(Exist)
+                .WithMessage("There is no publisher with the specified 'PublisherId'");
+        }
+
+        public async Task<bool> Exist(string publisherId, CancellationToken cancellationToken)
+        {
+            return await _context.Publishers
+                .AnyAsync(publisher => publisher.Id == publisherId, cancellationToken);
+        }
+    }
+}
