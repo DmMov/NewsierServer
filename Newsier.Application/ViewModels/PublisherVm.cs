731b18442f0230cfb8ca941b8f2a9ad5f1d75996
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-05-01 20:51:12 +0300

    Changed a view model

--- Newsier.Application/ViewModels/PublisherVm.cs
+++ Newsier.Application/ViewModels/PublisherVm.cs
@@ -1,5 +1,7 @@
-﻿using Newsier.Application.Mappings;
+﻿using AutoMapper;
+using Newsier.Application.Mappings;
 using Newsier.Domain.Entities;
+using System.Globalization;
 
 namespace Newsier.Application.ViewModels
 {
@@ -11,5 +13,18 @@ namespace Newsier.Application.ViewModels
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
+        public string CreatedAt { get; set; }
+        public uint Publications { get; set; }
+        public uint Comments { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            DateTimeFormatInfo dateTimeFormatInfo = CultureInfo.GetCultureInfo("uk-UA").DateTimeFormat;
+
+            profile.CreateMap<Publisher, PublisherVm>()
+                .ForMember(x => x.CreatedAt, opt => opt.MapFrom(x => x.CreatedAt.ToString("dd MMMM, yyyy", dateTimeFormatInfo)))
+                .ForMember(x => x.Publications, opt => opt.MapFrom(x => (uint)x.Publications.Count))
+                .ForMember(x => x.Comments, opt => opt.MapFrom(x => (uint)x.Comments.Count));
+        }
     }
 }
