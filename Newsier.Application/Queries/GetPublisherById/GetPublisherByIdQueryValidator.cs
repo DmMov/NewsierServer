cfc67e702474159c7fffa60185c0603d4c924b8f
DmMov <dmmov.live@gmail.com>
2020-07-02 21:23:28 +0300

    Fixed an issue

--- Newsier.Application/Queries/GetPublisherById/GetPublisherByIdQueryValidator.cs
+++ Newsier.Application/Queries/GetPublisherById/GetPublisherByIdQueryValidator.cs
@@ -1,30 +1,22 @@
 ﻿using FluentValidation;
-using Microsoft.EntityFrameworkCore;
 using Newsier.Application.Interfaces;
-using System.Threading;
-using System.Threading.Tasks;
+using Newsier.Domain.Entities;
 
 namespace Newsier.Application.Queries.GetPublisherById
 {
     public sealed class GetPublisherByIdQueryValidator : AbstractValidator<GetPublisherByIdQuery>
     {
-        private readonly INewsierContext _context;
+        private readonly IEntitiesSearchService _entitiesSearchService;
 
-        public GetPublisherByIdQueryValidator(INewsierContext context)
+        public GetPublisherByIdQueryValidator(IEntitiesSearchService entitiesSearchService)
         {
-            _context = context;
+            _entitiesSearchService = entitiesSearchService;
 
             RuleFor(query => query.PublisherId)
                 .NotEmpty()
                 .WithMessage("'PublisherId' is null or an empty string")
-                .MustAsync(Exist)
+                .MustAsync(_entitiesSearchService.ExistAsync<Publisher>)
                 .WithMessage("There is no publisher with the specified 'PublisherId'");
-        }
-
-        public async Task<bool> Exist(string publisherId, CancellationToken cancellationToken)
-        {
-            return await _context.Publishers
-                .AnyAsync(publisher => publisher.Id == publisherId, cancellationToken);
         }
     }
 }
